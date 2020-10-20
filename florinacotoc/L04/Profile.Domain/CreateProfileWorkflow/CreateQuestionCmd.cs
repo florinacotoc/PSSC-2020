ea1fb9086da284a38118b7d8400e4c33a1d9d8db
florinacotoc <florinaa19@gmail.com>
2020-10-20 20:49:38 +0300

    L04
    Tema4

---
+++ florinacotoc/L04/Profile.Domain/CreateProfileWorkflow/CreateQuestionCmd.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Question.Domain.CreateProfileWorkflow
+{
+    public struct CreateQuestionCmd
+    {
+        [Required]
+        public string Title { get; private set; }
+        public string Description { get; set; }
+        [Required]
+        public string Tag { get; private set; }
+
+        public CreateQuestionCmd(string title, string description, string tag)
+        {
+            Title = title;
+            Description = description;
+            Tag = tag;
+        }
+    }
+}
---
+++ florinacotoc/L04/Profile.Domain/CreateProfileWorkflow/CreateQuestionResult.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CSharp.Choices;
+
+namespace Question.Domain.CreateProfileWorkflow
+{
+    [AsChoice]
+    public static partial class CreateQuestionResult
+    {
+        public interface ICreateQuestionResult { }
+
+        public class QuestionCreated : ICreateQuestionResult
+        {
+            public string Title_q { get; private set; }
+            public string Description_q { get; private set; }
+
+            public ProfileCreated(string title_q, string description_q)
+            {
+                Title_q = title_q;
+                Description_q = description_q;
+            }
+        }
+
+        public class QuestionNotCreated : ICreateQuestionResult
+        {
+            public string Reason { get; set; }
+
+            public QuestionNotCreated(string reason)
+            {
+                Reason = reason;
+            }
+        }
+
+        public class QuestionValidationFailed : ICreateQuestionResult
+        {
+            public IEnumerable<string> ValidationErrors { get; private set; }
+
+            public QuestionValidationFailed(IEnumerable<string> errors)
+            {
+                ValidationErrors = errors.AsEnumerable();
+            }
+        }
+    }
+}
