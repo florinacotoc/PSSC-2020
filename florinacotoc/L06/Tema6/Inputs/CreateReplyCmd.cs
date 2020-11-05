07473667805c9eba47f5fef13c3eb82415e14696
florinacotoc <florinaa19@gmail.com>
2020-11-05 21:08:25 +0200

    L06
    Tema L06

---
+++ florinacotoc/L06/Tema6/Inputs/CreateReplyCmd.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tema6.Inputs
+{
+    public class CreateReplyCmd
+    {
+        public int AuthorId { get; }
+        public int QuestionId { get; }
+        public string Reply { get; }
+
+        public CreateReplyCmd(int authorId, int questionId, string reply)
+        {
+            AuthorId = authorId;
+            QuestionId = questionId;
+            Reply = reply;
+        }
+    }
+}
---
+++ florinacotoc/L06/Tema6/Outputs/CreateReplyResult.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Tema6.Models;
+using Tema6.Inputs;
+using CSharp.Choices;
+
+namespace Tema6.Outputs
+{
+    [AsChoice]
+    public static partial class CreateReplyResult
+    {
+        public interface IPublishReplyResult { }
+
+        public class ReplyValid : IPublishReplyResult
+        {
+            public Reply Reply { get; }
+
+            public ReplyValid(Reply reply)
+            {
+                Reply = reply;
+            }
+        }
+
+        public class ReplyInvalid : IPublishReplyResult
+        {
+            public string Reasons { get; }
+
+            public ReplyInvalid(string reasons)
+            {
+                Reasons = reasons;
+            }
+        }
+        public class InvalidRequest : IPublishReplyResult
+        {
+            public string ValidationErrors { get; }
+            public CreateReplyCmd Cmd { get; }
+            public InvalidRequest(string validationErrors, CreateReplyCmd cmd)
+            {
+                ValidationErrors = validationErrors;
+                Cmd = cmd;
+            }
+        }
+
+    }
+}
